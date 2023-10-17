1191cd59a9b3894edf74945c5b2b6a83a6e41eaa
miluski <sprytnykowal@gmail.com>
2023-10-17 22:14:23 +0200

    Initialize

---
+++ src/main/java/com/projects/korporacjataxi/HelloController.java
@@ -0,0 +1,22 @@
+package com.projects.korporacjataxi;
+
+import com.sothawo.mapjfx.*;
+import com.sothawo.mapjfx.MapView;
+import com.sothawo.mapjfx.offline.OfflineCache;
+import javafx.fxml.FXML;
+
+public class HelloController {
+    @FXML
+    private MapView mapView;
+
+    public void initMapAndControls(Projection projection) {
+        MapType mapType = MapType.OSM;
+        mapView.setMapType(mapType);
+        mapView.initialize(Configuration.builder()
+            .projection(projection)
+            .showZoomControls(false)
+            .build());
+        mapView.setZoom(14);
+        mapView.setCenter(new Coordinate(50.878978332517214, 20.640200536391333));
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.projects.korporacjataxi {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+    requires com.sothawo.mapjfx;
+    requires org.slf4j;
+
+    opens com.projects.korporacjataxi to javafx.fxml, javafx.graphics;
+    exports com.projects.korporacjataxi;
+}
