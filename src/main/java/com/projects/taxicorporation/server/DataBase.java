acf1df9d7abedbd56d4aad351bb5e796f90cbd4f
miluski <sprytnykowal@gmail.com>
2024-01-01 21:46:17 +0100

    Added DataBase.java back

---
+++ src/main/java/com/projects/taxicorporation/server/DataBase.java
@@ -0,0 +1,13 @@
+package com.projects.taxicorporation.server;
+
+import java.sql.*;
+
+public class DataBase extends AbstractDataBase {
+    public void connect () throws SQLException, ClassNotFoundException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "admin");
+    }
+    public void closeConnect() throws SQLException {
+        connect.close();
+    }
+}
