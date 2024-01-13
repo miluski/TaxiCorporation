8a1ca5d546f60f087e24fe7a4a7a68af0bc0e7fa
Pawel <webloopbox@gmail.com>
2024-01-13 18:59:50 +0100

    change db props

--- src/main/java/com/projects/taxicorporation/server/DataBase.java
+++ src/main/java/com/projects/taxicorporation/server/DataBase.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DataBase extends AbstractDataBase {
     public void connect () throws SQLException, ClassNotFoundException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
-        connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/xe", "john", "abcd1234");
+        connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "admin");
     }
     public void closeConnect() throws SQLException {
         connect.close();
