7ef195447e303755f5e875ade403339e274609e4
miluski <maksymilian.sowula@gmail.com>
2024-01-18 17:21:23 +0100

    Prototype project pattern unit tests implementation

---
+++ src/main/test/com/projects/taxicorporation/client/WhiteButtonPrototypeTest.java
@@ -0,0 +1,39 @@
+package com.projects.taxicorporation.client;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhiteButtonPrototypeTest {
+
+    @Test
+    void clone_shouldReturnCloneWithSameProperties() throws CloneNotSupportedException {
+        WhiteButtonPrototype original = new WhiteButtonPrototype();
+        original.setText("Original Text");
+        original.setWidth(100.0);
+        original.setHeight(30.0);
+        original.setX(50.0);
+        original.setY(20.0);
+        WhiteButtonPrototype clone = (WhiteButtonPrototype) original.clone();
+        assertNotSame(original, clone);
+        assertEquals(original.getText(), clone.getText());
+        assertEquals(original.getWidth(), clone.getWidth());
+        assertEquals(original.getHeight(), clone.getHeight());
+        assertEquals(original.getX(), clone.getX());
+        assertEquals(original.getY(), clone.getY());
+    }
+
+    @Test
+    void accessors_and_mutators_shouldWorkCorrectly() {
+        WhiteButtonPrototype button = new WhiteButtonPrototype();
+        button.setText("Test Text");
+        button.setWidth(80.0);
+        button.setHeight(25.0);
+        button.setX(40.0);
+        button.setY(15.0);
+        assertEquals("Test Text", button.getText());
+        assertEquals(80.0, button.getWidth());
+        assertEquals(25.0, button.getHeight());
+        assertEquals(40.0, button.getX());
+        assertEquals(15.0, button.getY());
+    }
+}
